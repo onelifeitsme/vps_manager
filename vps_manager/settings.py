1ecaf1eeec44b96dda1ab6a3fddc9435a26e26a5
Artur Karpovich <onelifeitsme@gmail.com>
2022-10-10 22:10:01 +0300

    added docs

--- vps_manager/settings.py
+++ vps_manager/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 MIDDLEWARE = [
