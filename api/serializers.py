b8779ca423131bf6064562e81b9c55903ecba242
Artur Karpovich <onelifeitsme@gmail.com>
2022-10-11 08:17:27 +0300

    added new serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,19 @@ from rest_framework import serializers
 from vps.models import VPS
 
 
-class VPSSerializer(serializers.ModelSerializer):
+class BasicVPSSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = VPS
+        fields = '__all__'
+        extra_kwargs = {
+            'cpu': {"read_only": True},
+            'ram': {"read_only": True},
+            'hdd': {"read_only": True},
+        }
+
+
+class FullVPSSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = VPS
--- api/views.py
+++ api/views.py
@@ -1,16 +1,16 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
-from .serializers import VPSSerializer
+from .serializers import BasicVPSSerializer, FullVPSSerializer
 from vps.models import VPS
 
 
 class VSPAPIView(ListCreateAPIView):
     queryset = VPS.objects.all()
-    serializer_class = VPSSerializer
+    serializer_class = FullVPSSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ('cpu', 'ram', 'hdd', 'status',)
 
 
 class SingleVPSAPIView(RetrieveUpdateAPIView):
     queryset = VPS.objects.all()
-    serializer_class = VPSSerializer
+    serializer_class = BasicVPSSerializer
